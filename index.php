951c7510df59e81282a222d00303bc2a1102c8b4
liuxiang <lx1036@126.com>
2016-05-27 18:20:20 +0800

    PHPUnit Book Learning End

--- index.php
+++ index.php
@@ -5,4 +5,16 @@
  * Date: 16/5/25
  * Time: 22:05
  */
-echo 'eloquent1.0.0 test';
+
+require_once __DIR__.'/vendor/autoload.php';
+
+use Illuminate\Database\Query\Builder;
+use Mockery as m;
+
+$interface = m::mock('Illuminate\Database\ConnectionInterface');
+$grammar = new \Illuminate\Database\Query\Grammars\Grammar();
+$processor = m::mock('Illuminate\Database\Query\Processors\Processor');
+$builder = new Builder($interface, $grammar, $processor);
+
+$sql = $builder->select('*')->from('char_test')->toSql();
+var_dump($sql);
