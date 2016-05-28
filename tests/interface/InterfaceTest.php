eb854bb422e897877a0d007aec1aace2acf7f225
liuxiang <lx1036@126.com>
2016-05-28 23:31:27 +0800

    Implement Interface ShortCut

---
+++ tests/interface/InterfaceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: liuxiang
+ * Date: 16/5/28
+ * Time: 下午11:26
+ */
+interface InterfaceTest
+{
+    public function run();
+    public function jump();
+}
---
+++ tests/interface/Person.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: liuxiang
+ * Date: 16/5/28
+ * Time: 下午11:27
+ */
+class Person implements InterfaceTest
+{
+
+    public function run()
+    {
+        // TODO: Implement run() method.
+        echo 'run';
+    }
+
+    public function jump()
+    {
+        // TODO: Implement jump() method.
+        echo 'jump';
+    }
+}
